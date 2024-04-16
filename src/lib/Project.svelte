49686d1333920bf98903f29f3bc9c405df390b12
olinaliva <lolina@mit.edu>
2024-04-16 18:09:16 -0400

    url working

--- src/lib/Project.svelte
+++ src/lib/Project.svelte
@@ -2,13 +2,16 @@
     export let info = {};
     // let p = info;
     export let hLevel = 2;
+    console.log(info.url);
 </script>
 
 <article>   
     <svelte:element this={"h" + hLevel}>{ info.title }</svelte:element>
     <img src={info.image} alt="">
-    <p><href>{info.url}</href></p>
-    <div><p>{info.description}</p>
+    
+    <div>
+        <a href={info.url}>link</a>
+        <p>{info.description}</p>
     <p>{info.year}</p></div>
 </article>
 
